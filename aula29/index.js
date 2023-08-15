d0ea57c34beb01bd91fb2f8f42ab859ab20c127b
Marcelo Leal <prasempreleal@gmail.com>
2023-08-15 11:58:58 -0300

    feat: adicionando parte da aula 29

---
+++ aula29/index.js
@@ -0,0 +1,25 @@
+const data = new Date('1977-03-11 14:00:00')
+let diaSemana = data.getDay();
+let diaSemanaTexto;
+
+switch (diaSemana){
+    case 0:
+        diaSemanaTexto = 'Domingo';
+        break;
+    case 1:
+        diaSemanaTexto = 'Segundo';
+        break;
+    case 2:
+        diaSemanaTexto = 'Terça'
+        break;
+    case 3:
+        diaSemanaTexto = 'Quarta'
+        break;
+    case 4:
+        diaSemanaTexto = 'Quinta'
+        break;
+    case 5: 
+        diaSemanaTexto = 'Sexta'
+        break;
+            
+}
